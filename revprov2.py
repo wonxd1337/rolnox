2d4cac36944300ee20db4e3d103c58a65ac060b0
WonXd2 <56256945+wonxd1337@users.noreply.github.com>
2026-03-16 06:40:26 +0700

    Update revprov2.py

--- revprov2.py
+++ revprov2.py
@@ -419,7 +419,7 @@ class MovableTypeScanner:
 
 def download_proxy_list():
     """Download proxy list dari GitHub langsung ke memory"""
-    proxy_url = "https://raw.githubusercontent.com/proxifly/free-proxy-list/refs/heads/main/proxies/countries/SG/data.txt"
+    proxy_url = "https://raw.githubusercontent.com/proxifly/free-proxy-list/refs/heads/main/proxies/all/data.txt"
     
     try:
         print("[*] Mendownload proxy list...")
